aa78eeab7c096565177218c864ffe850c44731d6
Andy Weiss <andyweiss1982@gmail.com>
2020-11-10 19:31:05 +0000

    eslint

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const express = require("express")
-const { Pool } = require("pg")
+const express = require('express')
+const { Pool } = require('pg')
 
 const app = express()
 const port = 3000
 
 let connection = {
-	host: 'localhost',
-	port: 5432, 
-	database: 'postgres', 
-	user: 'postgres', 
-	password: 'postgres' 
+  host: 'localhost',
+  port: 5432,
+  database: 'postgres',
+  user: 'postgres',
+  password: 'postgres'
 }
 
-if (process.env.DATABASE_URL){
+if (process.env.DATABASE_URL) {
   connection = { connectionString: process.env.DATABASE_URL }
 }
 
 const db = new Pool(connection)
 
-app.get("/", async (_, response) => {
-  const { rows } = await db.query(`SELECT NOW();`)      
+app.get('/', async (_, response) => {
+  const { rows } = await db.query('SELECT NOW();')
   response.send(`<h1>The current database time is ${rows[0].now}</h1>`)
 })
 
